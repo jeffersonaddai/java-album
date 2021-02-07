a347ccd8d92b9a4e396ceca553d22a2213f36ba6
jefferson <jeffepok@gmail.com>
2021-02-07 14:43:42 +0000

    added functionality to the Song class

--- src/Album.java
+++ src/Album.java
@@ -10,39 +10,43 @@ public class Album {
         this.artist = artist;
         songs = new ArrayList<>();
     }
-    public boolean addSong(String titleOfSong, double durationOfSong){
-        Song newSong= new Song(titleOfSong, durationOfSong);
-        if(findSong(titleOfSong) == null){
+
+    public boolean addSong(String titleOfSong, double durationOfSong) {
+        Song newSong = new Song(titleOfSong, durationOfSong);
+        if (findSong(titleOfSong) == null) {
             songs.add(newSong);
             return true;
-        }
-        else return false;
+        } else return false;
     }
-    private Song findSong(String titleOfSong){
-        for(int i = 0; i < songs.size(); i++){
-            if(songs.get(i).getTitle().equals(titleOfSong)) {
+
+    private Song findSong(String titleOfSong) {
+        for (int i = 0; i < songs.size(); i++) {
+            if (songs.get(i).getTitle().equals(titleOfSong)) {
                 return songs.get(i);
             }
         }
         return null;
     }
 
-    public boolean addToPlayList(int trackNumberOfSong, LinkedList<Song> playlist){
-        if(trackNumberOfSong < songs.size() && trackNumberOfSong > -1){
+    public boolean addToPlayList(int trackNumberOfSong, LinkedList<Song> playlist) {
+        if (trackNumberOfSong < songs.size() && trackNumberOfSong > -1) {
             playlist.add(songs.get(trackNumberOfSong));
             return true;
         }
         return false;
     }
-    public boolean addToPlayList(String titleOfSong, LinkedList<Song> playlist){
-        if(findSong(titleOfSong) != null){
+
+    public boolean addToPlayList(String titleOfSong, LinkedList<Song> playlist) {
+        if (findSong(titleOfSong) != null) {
             playlist.add(findSong(titleOfSong));
             return true;
         }
         return false;
     }
-    public void printSongs(){
-        for(Song song: songs){
+
+    public void printSongs() {
+        for (Song song : songs) {
             System.out.println(song.toString());
         }
     }
+}
